831e30a4011db1c890dd0072f11079b691157ef8
Hatim Lagzar <lhatimdev@gmail.com>
2022-12-31 13:31:34 +0100

    fix subscription success route url

--- src/components/Router/Router.jsx
+++ src/components/Router/Router.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Route, Routes} from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Login from "../../pages/login";
 import Index from "../../pages";
 import Register from "../../pages/register";
@@ -18,20 +18,20 @@ import ResetPassword from "../../pages/reset-password/[token]";
 export default function Router() {
   return (
     <Routes>
-      <Route path='login' exact element={<Login/>}/>
-      <Route path='register' exact element={<Register/>}/>
-      <Route path='/' exact element={<Index/>}/>
-      <Route path='delegator' exact element={<Delegator/>}/>
-      <Route path='start' exact element={<Start/>}/>
-      <Route path='subscribe' exact element={<Subscribe/>}/>
-      <Route path='subscribe-success' exact element={<SubscribeSuccess/>}/>
-      <Route path='recover' exact element={<Recover/>}/>
-      <Route path='reset-password/:token' exact element={<ResetPassword/>}/>
-      <Route path='meditation' exact element={<Meditation/>}/>
-      <Route path='map' exact element={<Map/>}/>
-      <Route path='calendar' exact element={<CalendarIndex/>}/>
-      <Route path='calendar/create' exact element={<CalendarCreate/>}/>
-      <Route path='calendar/:id' exact element={<CalendarEdit/>}/>
+      <Route path='login' exact element={<Login />} />
+      <Route path='register' exact element={<Register />} />
+      <Route path='/' exact element={<Index />} />
+      <Route path='delegator' exact element={<Delegator />} />
+      <Route path='start' exact element={<Start />} />
+      <Route path='subscribe' exact element={<Subscribe />} />
+      <Route path='subscription-success' exact element={<SubscribeSuccess />} />
+      <Route path='recover' exact element={<Recover />} />
+      <Route path='reset-password/:token' exact element={<ResetPassword />} />
+      <Route path='meditation' exact element={<Meditation />} />
+      <Route path='map' exact element={<Map />} />
+      <Route path='calendar' exact element={<CalendarIndex />} />
+      <Route path='calendar/create' exact element={<CalendarCreate />} />
+      <Route path='calendar/:id' exact element={<CalendarEdit />} />
     </Routes>
   );
 }
