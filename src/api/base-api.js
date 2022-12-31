23299da93e386cab1269ec47350ac7a0bd0ddad4
Hatim Lagzar <lhatimdev@gmail.com>
2022-12-31 14:45:09 +0100

    fix getting subs sucess page for production

--- src/api/base-api.js
+++ src/api/base-api.js
@@ -6,3 +6,12 @@ export const getBaseApiUrl = () => {
     return 'https://api.ideamonster.net/api';
   }
 };
+
+export const getBaseUrl = () => {
+  const env = process.env.NODE_ENV;
+  if (env === 'development') {
+    return 'http://127.0.0.1:3000';
+  } else {
+    return 'https://ideamonster.net';
+  }
+};
--- src/utils/constants/urls.js
+++ src/utils/constants/urls.js
@@ -1,8 +1,5 @@
+import { getBaseUrl } from "../../api/base-api";
+
 export const RETURN_URL_AFTER_SUCCESSFUL_SUBSCRIPTION = (() => {
-  const env = process.env.NODE_ENV
-  if (env === "development") {
-    return 'http://127.0.0.1:3000/subscription-success';
-  } else if (env === "production") {
-    // do something
-  }
+  return getBaseUrl() + '/subscription-success';
 })();
